c27aa14db0a559c7ac169f821f20b4ae75fe5181
Swordax <88690330+SwordaxDev@users.noreply.github.com>
2022-01-03 19:31:29 +0400

    Add files via upload

---
+++ collegeboard automater/README.md
@@ -0,0 +1,72 @@
+# CVAA
+**Collegeboard Video Assignment Automator
+V1.0.0**
+
+<img alt="CVAA Logo" src="/docs/cvaa-logo.png" width="200">
+
+## About
+CVAA (Collegeboard Video Assignment Automator) is a free open source software 
+developed by Swordax, the software is written using JavaScript and is open source 
+on github.
+
+**Repo: https://github.com/SwordaxDev/CVAA**<br>
+*Software code is valid as of Jan 2022, collegeboard site (https://www.collegeboard.org)*
+
+CVAA when ran in the console, automates running the video assignments on 
+collegeboard AP courses, and makes sure all the assigned videos are completed.
+
+## Mechanism
+
+ - Finds the oldest un-completed video assignment in the assignments list
+ - Selects the assignment and sets the video speed to x2
+ - Plays the video when loaded
+ - As video ends, selects the next un-completed video assignment and triggers it
+ - As videos complete, CVAA keeps logging updates in the page devtools console
+ - When all assigned videos are automatically triggered and completed on max speed, CVAA ends task
+
+## Features
+
+ - Assignments completion automation
+ - Max speed performance
+ - Simplicity of use
+ - Instant action logger
+ - Free to use & open source
+
+## How to use
+
+ 1. Open collegeboard AP classroom assignments page
+ 2. Open the devtools by `right click` > `inspect` or by pressing the `f12` key
+ 3. Select the console tab in the devtools
+ 4. Paste the following code in the console
+```js
+fetch("https://raw.githubusercontent.com/SwordaxDev/CVAA/main/index.js")
+.then(res => res.text())
+.then(data => eval(data));
+```
+ 5. Press Enter
+ 6. Leave the window opened and have fun till all assignments are completed :)<br><br>
+![implementation example](/docs/implementation-example.png)
+
+## Handle Errors
+You might occasionally face some errors and unexpected actions, such as the 
+software not running properly or the videos not playing. Something you could do 
+that might sometimes fix such issues is refreshing the page and trying again.
+
+The software might not run properly if the collegeboard assignments looked 
+something like this:
+<br><br>
+![invalid example 1](/docs/invalid-example-1.png)
+![invalid example 2](/docs/invalid-example-2.png)
+<br><br>
+Try resizing the browser window or use a device with a larger screen then refresh your 
+window, if your assignments now look like this, then you are good to go!
+<br><br>
+![valid example](/docs/valid-example.png)
+<br><br>
+***Please note that fixing this issue will be a priority update for future versions of the software***
+
+## Report Bugs
+If you encountered any bug while using the software (other than the issues mentioned 
+in the handle errors section) then don't hesitate to report them to me directly by messaging 
+me on discord, find me as Swordax#5756
+<br><br>[<img alt="Discord Logo" src="/docs/discord-logo.png" width="100">](https://discord.com/users/465453058667839499/)<br><br>
---
+++ collegeboard automater/index.js
@@ -0,0 +1,283 @@
+(() => {
+  /*
+    CVAA (Collegeboard Video Assignment Automator)
+    Developed by Swordax
+    - my website: https://swordax.netlify.app/
+    - my github: https://github.com/SwordaxDev/
+    - my discord (Swordax#5756): https://discord.com/users/465453058667839499/
+    - project repo: https://github.com/SwordaxDev/CVAA
+    - current code production date: 3 Jan, 2022
+	*/
+
+  // code valid as of Jan 2022 collegeboard site (collegeboard.org)
+
+  // variables
+  import { data } from "./info";
+  const CVAA_VERSION = data.project_info.project_version;
+  const lessonsTitles = document.querySelectorAll(
+    ".StudentAssignments .assignment_title"
+  );
+  const intervalTime = 500;
+  let oldestBranch = (latestBranch = lessons = null);
+  const logStyle = (color) => {
+    return `font-size:20px;color:${color};`;
+  };
+
+  // get current time and format it (hh:mm)
+  function gimmeTime() {
+    let timeNow = new Date();
+    let hourNow = timeNow.getHours(),
+      minuteNow = timeNow.getMinutes();
+    let gimmeHour = () => {
+      if (hourNow == 0) return 12;
+      else if (hourNow > 12) return hourNow - 12;
+      else return hourNow;
+    };
+    let gimmeMinute = () => {
+      if (minuteNow.toString().length == 1) return `0${minuteNow}`;
+      else return minuteNow;
+    };
+    let gimmeNoon = () => {
+      if (hourNow > 11) return "PM";
+      else return "AM";
+    };
+    return `${gimmeHour()}:${gimmeMinute()} ${gimmeNoon()}`;
+  }
+
+  // as program runs, log success message
+  console.log(
+    `%cCVAA software version ${CVAA_VERSION} running successfully at ${gimmeTime()}`,
+    logStyle("lime")
+  );
+  console.log(
+    "%cCVAA software developed by Swordax#5756\nGithub: https://github.com/SwordaxDev/\nSoftware Repo: https://github.com/SwordaxDev/CVAA",
+    logStyle("cyan")
+  );
+
+  // crop lesson
+  function gimmeCropped(lesson) {
+    return {
+      title: lesson.substring(0, lesson.lastIndexOf(".")),
+      branch: lesson.substring(lesson.lastIndexOf(".") + 1),
+    };
+  }
+
+  // get oldest un-finished assignment
+  const gimmeLesson = (state) => {
+    let oldestChapter = (oldestLesson = latestChapter = latestLesson = null);
+    lessons = [];
+    // get all lesson identifications/names
+    lessonsTitles.forEach((title) => {
+      if (!title.innerText.includes("Daily Video")) return false;
+      let lessonName = title.innerText;
+      let lessonNum = lessonName.split(":")[0];
+      let lessonBranch = lessonName.split(":")[1].replace(" Daily Video ", "");
+      lessons.push(`${lessonNum}.${lessonBranch}`);
+    });
+    // get oldest and latest chapter
+    lessons.forEach((lesson) => {
+      let croppedLesson = gimmeCropped(lesson).title.split(".");
+      let chapNum = parseInt(croppedLesson[0]);
+      if (!oldestChapter || chapNum < oldestChapter) oldestChapter = chapNum;
+      if (!latestChapter || chapNum > latestChapter) latestChapter = chapNum;
+    });
+    // get oldest and latest lesson
+    lessons.forEach((lesson) => {
+      let croppedLesson = gimmeCropped(lesson).title.split(".");
+      let chapNum = croppedLesson[0];
+      let lessonNum = parseInt(croppedLesson[1]);
+      if (chapNum == oldestChapter) {
+        if (!oldestLesson || lessonNum < oldestLesson) oldestLesson = lessonNum;
+      } else if (chapNum == latestChapter) {
+        if (!latestLesson || lessonNum > latestLesson) latestLesson = lessonNum;
+      }
+    });
+    // get oldest and latest branch
+    lessons.forEach((lesson) => {
+      let { title, branch } = gimmeCropped(lesson);
+      if (title == `${oldestChapter}.${oldestLesson}`) {
+        if (!oldestBranch || branch < oldestBranch) oldestBranch = branch;
+      } else if (title == `${latestChapter}.${latestLesson}`) {
+        if (!latestBranch || branch > latestBranch) latestBranch = branch;
+      }
+    });
+    // return latest/oldest un-finished video assignment identification/name
+    if (state == "latest") return `${latestChapter}.${latestLesson}`;
+    else if (state == "oldest") return `${oldestChapter}.${oldestLesson}`;
+    else
+      return console.log(
+        "%cCVAA ERROR: gimmeLesson() state is undefined",
+        logStyle("crimson")
+      );
+  };
+
+  // check if no videos are assigned
+  let assignmentsFound = false;
+  lessonsTitles.forEach((title) => {
+    if (title.innerText.includes("Daily Video")) assignmentsFound = true;
+  });
+  if (!assignmentsFound)
+    return console.log(
+      "%cCVAA: No video assignments found",
+      logStyle("yellow")
+    );
+
+  // open oldest video assignment
+  lessonsTitles.forEach((title) => {
+    let oldestLessonTitle = gimmeLesson("oldest");
+    if (
+      title.innerText.split(":")[0] == oldestLessonTitle &&
+      title.innerText.includes("Daily Video")
+    ) {
+      title.click();
+      // trigger video
+      triggerVideo(oldestLessonTitle, oldestBranch);
+      // check if video is playing
+      let videoPlayingChecker = setInterval(() => {
+        if (!isPlaying) {
+          triggerVideo(
+            `${nextVideoChapter}.${nextVideoLesson}`,
+            nextVideoBranch
+          );
+        } else {
+          clearInterval(videoPlayingChecker);
+        }
+      }, intervalTime);
+    }
+  });
+
+  // open next video assignment
+  let incBy = 1;
+  function openNext() {
+    // get indexes and next video
+    let currentIndex = document
+      .querySelector(".is-selected")
+      .getAttribute("data-index");
+    let nextIndex = parseInt(currentIndex) + incBy;
+    let nextVideo = document.querySelector(`button[data-index="${nextIndex}"]`);
+    if (nextVideo) {
+      // get lesson names sauses
+      let nextVideoFullTitle =
+        nextVideo.querySelector(".ft-video-title").innerText;
+      let nextVideoBranch = parseInt(
+          nextVideoFullTitle.split(":")[1].replace(" Daily Video ", "")
+        ),
+        nextVideoTitle = nextVideoFullTitle.split(":")[0].split("."),
+        latestVideoTitle = gimmeLesson("latest").split(".");
+      // parse destructured sauses
+      let nextVideoChapter = parseInt(nextVideoTitle[0]),
+        nextVideoLesson = parseInt(nextVideoTitle[1]),
+        latestVideoChapter = parseInt(latestVideoTitle[0]),
+        latestVideoLesson = parseInt(latestVideoTitle[1]);
+      // check if next video passed the latest assigned video
+      if (
+        nextVideoChapter > latestVideoChapter ||
+        (nextVideoChapter == latestVideoChapter &&
+          nextVideoLesson > latestVideoLesson) ||
+        (nextVideoChapter == latestVideoChapter &&
+          nextVideoLesson == latestVideoLesson &&
+          nextVideoBranch > latestBranch)
+      ) {
+        console.log(
+          `%cCVAA: Congrats! all video assignments are completed at ${gimmeTime()}`,
+          logStyle("lime")
+        );
+      } else {
+        // check if video required
+        let videoRequired = lessons.some(
+          (el) =>
+            el == `${nextVideoChapter}.${nextVideoLesson}.${nextVideoBranch}`
+        );
+        if (!videoRequired) {
+          incBy++;
+          return openNext();
+        } else {
+          incBy = 1;
+        }
+        nextVideo.click();
+        // check if next video is selected yet
+        let checkVideo = setInterval(() => {
+          if (nextVideo.classList.contains("is-selected")) {
+            clearInterval(checkVideo);
+            // trigger next video
+            triggerVideo(
+              `${nextVideoChapter}.${nextVideoLesson}`,
+              nextVideoBranch
+            );
+            // check if video is playing
+            let videoPlayingChecker = setInterval(() => {
+              if (!isPlaying) {
+                triggerVideo(
+                  `${nextVideoChapter}.${nextVideoLesson}`,
+                  nextVideoBranch
+                );
+              } else {
+                clearInterval(videoPlayingChecker);
+              }
+            }, intervalTime);
+          }
+        }, intervalTime);
+      }
+    } else {
+      // if next video was not found, print error
+      console.log("%cCVAA ERROR: next video not found :(", logStyle("crimson"));
+    }
+  }
+
+  // trigger video function
+  function triggerVideo(lessonTitle = "unknown", lessonBranch = "unknown") {
+    // open video settings
+    let settingsOpener = setInterval(() => {
+      let settingsBtn = document.querySelector("[title='Show settings menu']");
+      if (settingsBtn) {
+        clearInterval(settingsOpener);
+        settingsBtn.click();
+        // set video speed to x2
+        let speedClicker = setInterval(() => {
+          let speedBtn = document.querySelector("[for='2x']");
+          if (speedBtn) {
+            clearInterval(speedClicker);
+            speedBtn.click();
+            // play current video
+            let videoPlayer = setInterval(() => {
+              let playBtn = document.querySelector("[title='Play Video']");
+              if (playBtn) {
+                clearInterval(videoPlayer);
+                playBtn.click();
+                // check if video finished
+                let videoGetter = setInterval(() => {
+                  let videoEl = document.querySelector(
+                    ".w-video-wrapper video"
+                  );
+                  if (videoEl) {
+                    clearInterval(videoGetter);
+                    videoEl.onended = () => {
+                      console.log(
+                        `%cCVAA: Lesson ${lessonTitle} Daily Video ${lessonBranch} is successfully completed at ${gimmeTime()}!`,
+                        logStyle("lime")
+                      );
+                      // open the next video assignment
+                      openNext();
+                    };
+                  }
+                }, intervalTime);
+              }
+            }, intervalTime);
+          }
+        }, intervalTime);
+      }
+    }, intervalTime);
+  }
+
+  // check if video is playing
+  function isPlaying() {
+    let videoEl = document.querySelector(".w-video-wrapper video");
+    let videoPlaying = !!(
+      videoEl.currentTime > 0 &&
+      !videoEl.paused &&
+      !videoEl.ended &&
+      videoEl.readyState > 2
+    );
+    return videoPlaying;
+  }
+})();
